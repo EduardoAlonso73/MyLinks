52f13c5442346a2f31549507700c5176cb4e6d9b
Eduardo <eduardo.alonso.73h@gmail.com>
2022-12-07 21:33:51 -0600

    We setup binding,setupViewModel and setupRecyclerView

--- app/src/main/java/com/example/savelink/ui/favoriteModule/FavoriteFragment.kt
+++ app/src/main/java/com/example/savelink/ui/favoriteModule/FavoriteFragment.kt
@@ -1,21 +1,66 @@
 package com.example.savelink.ui.favoriteModule
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.savelink.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.savelink.data.entities.LinkEnt
+import com.example.savelink.databinding.FragmentFavoriteBinding
+import com.example.savelink.ui.favoriteModule.Adapter.LinkFavoriteAdapter
+import com.example.savelink.ui.favoriteModule.favoriteViewModel.LinkFavoriteViewModel
+import com.example.savelink.utils.IOnClickListener
 
 
-class favoriteFragment : Fragment() {
+class FavoriteFragment : Fragment(),IOnClickListener {
+    private lateinit var mAdapter: LinkFavoriteAdapter
+    private lateinit var mGridLayout: GridLayoutManager
+    private lateinit var mBinding: FragmentFavoriteBinding
+    private lateinit var viewModelProvide: LinkFavoriteViewModel
+    private lateinit var appContext: Context
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        appContext = requireContext().applicationContext
+        mBinding=FragmentFavoriteBinding.inflate(inflater,container,false)
+        return mBinding.root
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setupViewModel()
+    }
+
+
+    private fun setupRecyclerView() {
+        mAdapter = LinkFavoriteAdapter(this)
+        mGridLayout = GridLayoutManager(appContext, 1)
+        mBinding.recyclerviewfv.apply {
+            setHasFixedSize(true)
+            layoutManager = mGridLayout
+            adapter = mAdapter
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModelProvide = ViewModelProvider(requireActivity())[LinkFavoriteViewModel::class.java]
+        viewModelProvide.getListLinkFavori().observe(viewLifecycleOwner) {
+            mAdapter.submitList(it)
+        }
+    }
+
+    override fun onClick(linkEnt: LinkEnt) {}
+
+    override fun isFavorite(linkEnt: LinkEnt) {}
+
+    override fun removeLink(linkEnt: LinkEnt) {}
+
+    override fun shareLink(link: String) {}
+
+    override fun copiClipboard(link: String) {}
 }
