9a07a2a5e955e39247e84e3365af8cf403b496ed
Eduardo <eduardo.alonso.73h@gmail.com>
2022-12-08 12:48:47 -0600

    We create the  category module

---
+++ app/src/main/java/com/example/savelink/ui/categoryModule/Adapter/CategoryAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.savelink.ui.categoryModule.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.savelink.R
+import com.example.savelink.data.entities.CategoryEnt
+import com.example.savelink.data.entities.LinkEnt
+import com.example.savelink.databinding.ItemCategoryBinding
+import com.example.savelink.databinding.ItemLinkBinding
+import com.example.savelink.utils.AuxOpenGraphCallback
+import com.example.savelink.utils.IOnClickListener
+
+
+class CategoryAdapter() :
+    ListAdapter<CategoryEnt, RecyclerView.ViewHolder>(StoreDiffCallback()) {
+    private lateinit var mContext: Context
+    private lateinit var binding: ItemCategoryBinding
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        mContext = parent.context
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
+        binding = ItemCategoryBinding.bind(view)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val itemLinks = getItem(position)
+        with(holder as ViewHolder) {
+            with(binding) {
+                setListener(itemLinks)
+                binding.chipChat.text = itemLinks.category
+
+            }
+
+        }
+    }
+
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ItemCategoryBinding.bind(view)
+        fun setListener(categoryEnt: CategoryEnt) {
+
+        }
+    }
+
+
+    class StoreDiffCallback : DiffUtil.ItemCallback<CategoryEnt>() {
+        override fun areItemsTheSame(oldItem: CategoryEnt, newItem: CategoryEnt): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: CategoryEnt, newItem: CategoryEnt): Boolean =
+            oldItem == newItem
+    }
+
+
+}
+
