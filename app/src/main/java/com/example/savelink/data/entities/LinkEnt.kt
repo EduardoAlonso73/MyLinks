e67fbbbcd20ffa02b9035f04de0dd31284bf9c48
Eduardo <eduardo.alonso.73h@gmail.com>
2022-12-08 21:51:54 -0600

    We added the category property in the LinkEnt

--- app/src/main/java/com/example/savelink/data/entities/LinkEnt.kt
+++ app/src/main/java/com/example/savelink/data/entities/LinkEnt.kt
@@ -3,8 +3,10 @@ package com.example.savelink.data.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName ="SaveLinkEnt" )
+@Entity(tableName = "SaveLinkEnt")
 data class LinkEnt(
-   @PrimaryKey(autoGenerate = true) var id:Long=0,
-    var link:String,
-    var isFavorite:Boolean=false)
+    @PrimaryKey(autoGenerate = true) var id: Long = 0,
+    var link: String,
+    var category: String = "",
+    var isFavorite: Boolean = false
+)
