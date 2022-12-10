b193c6e352825bbb05883bed62a203ccc6df0f47
Eduardo <eduardo.alonso.73h@gmail.com>
2022-12-10 15:33:58 -0600

    We added suspend  to the function

--- app/src/main/java/com/example/savelink/damain/GetListByCategoryCaseUsen.kt
+++ app/src/main/java/com/example/savelink/damain/GetListByCategoryCaseUsen.kt
@@ -1,10 +1,12 @@
 package com.example.savelink.damain
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.savelink.data.database.entities.SaveLinkRepository
 import com.example.savelink.data.entities.LinkEnt
 
-class GetListLinkCaseUsen {
+class GetListByCategoryCaseUsen {
     private val domainRepository by lazy { SaveLinkRepository()}
-     suspend fun getLink() =domainRepository.listAllLink()
+
+    suspend fun getLinkByCategory(category:String)=domainRepository.getLinkByCategory(category)
 }
--- app/src/main/java/com/example/savelink/damain/GetListCategoryCaseUsen.kt
+++ app/src/main/java/com/example/savelink/damain/GetListCategoryCaseUsen.kt
@@ -2,9 +2,10 @@ package com.example.savelink.damain
 
 import androidx.lifecycle.LiveData
 import com.example.savelink.data.database.entities.SaveLinkRepository
+import com.example.savelink.data.entities.CategoryEnt
 import com.example.savelink.data.entities.LinkEnt
 
-class GetListLinkCaseUsen {
+class GetListCategoryCaseUsen {
     private val domainRepository by lazy { SaveLinkRepository()}
-     suspend fun getLink() =domainRepository.listAllLink()
+    fun getCategory(): LiveData<MutableList<CategoryEnt>> =domainRepository.listAllCategory
 }
